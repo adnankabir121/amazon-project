0dc58c490a49209f3cf6d0e3cd8ffea661aff2df
adnankabir121 <adnankabir618@gmail.com>
2020-09-05 11:47:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter></Counter>
     </div>
   );
 }
+
+function Counter(){
+  const [count,setCount] = useState(7);
+  useEffect(()=>{
+    
+  },[]);
+
+
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }; 
+
+  return(
+    <div className="text-center py-4">
+      <h1>Count : {count}</h1>
+      <button className="btn btn-primary mt-3" onClick={handleIncrease}>Increase</button>
+    </div>
+    
+  );
+}
+
 
 export default App;
